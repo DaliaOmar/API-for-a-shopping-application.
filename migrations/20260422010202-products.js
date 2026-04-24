d66440a624c53edb4594fdf3d15fcfc915ea87ab
DaliaOmar <78263948+DaliaOmar@users.noreply.github.com>
2026-04-25 00:53:00 +0300

    Add files via upload

---
+++ migrations/20260422010202-products.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+ exports.up = function (db) {
+  return db.createTable('products', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    name: 'string',
+    price: 'decimal',
+    category: 'string'
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('products');
+};
+
+
+exports._meta = {
+  "version": 1
+};
